2b97a772fb5dd15c34e4060c9be959305e34c6d1
Cody Maheu <cmaheu@c02f70b1md6p.attlocal.net>
2021-06-28 13:24:16 -0700

    testing

--- views/pages/Home.js
+++ views/pages/Home.js
@@ -1,17 +1,17 @@
 let params = (new URL(document.location)).searchParams;
-let name = params.get("state");
-console.log(name)
+let state = params.get("state");
+console.log(state)
 
 const Home = {
   allowAccess: async () => true,
   render: async () => {
     const view = /*html*/ `
     <h3>Security Check Setup</h3>
-    <p>Using the TradeStation Mobile app ${name}</p>
+    <p>Using the TradeStation Mobile app</p>
     <div id="summary">
     <p>When you login from an untrusted device instead of being asked to enter a 6-digit passcode you will simply need to approve the login request with a tap of your finger.</p>
     <p>Even better, when you log in to the mobile app on this device we will very the login request for you, which gives you the same extra security without the extra step.</p>
-    <form action="https://google.com">
+    <form action="https://cmaheu-enterprise.us.auth0.com/continue?${state}">
     <input type="submit" value="Continue" />
     </form>
     <form action="https://google.com">
