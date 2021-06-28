ef04b7aab21ddd74ef568b4b42e44b1e609a0f89
Cody Maheu <cmaheu@c02f70b1md6p.attlocal.net>
2021-06-28 13:31:38 -0700

    test

--- views/pages/Home.js
+++ views/pages/Home.js
@@ -2,6 +2,9 @@ let params = (new URL(document.location)).searchParams;
 let state = params.get("state");
 console.log(state)
 
+let url = `https://cmaheu-enterprise.us.auth0.com/continue?${state}`
+console.log(url)
+
 const Home = {
   allowAccess: async () => true,
   render: async () => {
@@ -11,7 +14,7 @@ const Home = {
     <div id="summary">
     <p>When you login from an untrusted device instead of being asked to enter a 6-digit passcode you will simply need to approve the login request with a tap of your finger.</p>
     <p>Even better, when you log in to the mobile app on this device we will very the login request for you, which gives you the same extra security without the extra step.</p>
-    <form action="https://cmaheu-enterprise.us.auth0.com/continue?${state}">
+    <form action=${url} >
     <input type="submit" value="Continue" />
     </form>
     <form action="https://google.com">
