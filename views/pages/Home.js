baf9ac24dbd800ccc6ae3187d1790c15cc53e7a3
Cody Maheu <cmaheu@c02f70b1md6p.attlocal.net>
2021-06-28 13:20:25 -0700

    try

--- views/pages/Home.js
+++ views/pages/Home.js
@@ -7,7 +7,7 @@ const Home = {
   render: async () => {
     const view = /*html*/ `
     <h3>Security Check Setup</h3>
-    <p>Using the TradeStation Mobile app</p>
+    <p>Using the TradeStation Mobile app ${name}</p>
     <div id="summary">
     <p>When you login from an untrusted device instead of being asked to enter a 6-digit passcode you will simply need to approve the login request with a tap of your finger.</p>
     <p>Even better, when you log in to the mobile app on this device we will very the login request for you, which gives you the same extra security without the extra step.</p>
